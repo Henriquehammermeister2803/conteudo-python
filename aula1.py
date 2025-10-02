629536ae407a9d98d0df01e31d14a69e8b97aec8
Aluno SENAI <Aluno@SN-345403.edu.fiesc.com.br>
2025-10-02 16:48:24 -0300

    conteudo sobre print e variaveis

---
+++ aula1.py
@@ -0,0 +1,12 @@
+# objetivo do primeiro código, escapar da maldição
+print("olá mundo")
+# abaixo está o comando print comsep e end padrão
+print("lógica","de","programação",sep=" ",end="\n")
+# abaixo cada concatenação é um enter (\n significa enter)
+print("vai imprimir em baixo?","\n")
+# abaixo cada comando possui o end alterado para imprimir na mesma linha
+print("vai",end=" ")
+print("imprimir",end=" ")
+print("na",end=" ")
+print("mesma",end=" ")
+print("linha",end=" ")
---
+++ aula1_variaveis.py
@@ -0,0 +1,9 @@
+# podemos definir váriaveis com o nome dela, seguido pelo sinalde igual
+# quesignifica "atribuir" e depois pelo valor.
+# nome = "henrique" <- crie uma váriavel nome que recebe o valor "henrique
+nome = "Henrique"
+salario = 100000.00
+dias_trabalhados = 1
+print("O funcionário é",nome)
+print("Ele traabalhou",dias_trabalhados, "dias")
+print("O salário é",salario),"(tipo: "type(salario)," ")
