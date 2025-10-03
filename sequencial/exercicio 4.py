f9017f64d1cf40595ed20f177584b1a3ba20a6cb
Aluno SENAI <Aluno@SN-345403.edu.fiesc.com.br>
2025-10-03 16:57:06 -0300

    exercicios

---
+++ sequencial/exercicio 4.py
@@ -0,0 +1,4 @@
+nota_1 = float(input("digite a primeira nota: "))
+nota_2 = float(input("digite a primeira nota: "))
+nota_3 = float(input("digite a primeira nota: "))
+nota_4 = float(input("digite a primeira nota: "))
